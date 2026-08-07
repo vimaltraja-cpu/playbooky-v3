393f7d79b585bf6ded30abdd519919c6c4a48070
vimaltraja-cpu <vimaltraja@gmail.com>
2026-08-07 15:57:25 +0100

    feat: add recommendation loading and recommendation reveal template screens

---
+++ app/recommendation-reveal-template/page.tsx
@@ -0,0 +1,5 @@
+import { RecommendationRevealTemplateExperience } from "@/components/product/RecommendationRevealTemplateExperience";
+
+export default function RecommendationRevealTemplatePage() {
+  return <RecommendationRevealTemplateExperience />;
+}
